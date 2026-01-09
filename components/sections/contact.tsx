bdb40eeb5b6467e24645f8c24dbae27816b29580
ButteWill <wcruttenden001@student.butte.edu>
2026-01-09 15:09:47 -0800

    fix form

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -70,7 +70,7 @@ export function Contact() {
         formObject[key] = value.toString()
       })
 
-      await fetch("/", {
+      await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encode({ "form-name": "contact", ...formObject }),
@@ -120,6 +120,7 @@ export function Contact() {
                 className="space-y-6"
                 name="contact"
                 method="POST"
+                action="/__forms.html"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
               >
